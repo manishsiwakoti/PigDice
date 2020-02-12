9220d2550f5886bdaa542d50515d5d465d9d139b
max-student <max-student@maxtrain.com>
2020-02-12 09:00:40 -0500

    Add project files.

---
+++ PigDice/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace PigDice
+    {
+    class Program
+        {
+        static void Main(string[] args)
+            {
+            var pigDice = new Dice();
+            pigDice.Play();
+            }
+        }
+    }
